e2fcf7265afb5b1e8b3bcedf3234ce91cd29ed6f
Komal Kaur <44416323+Komal914@users.noreply.github.com>
2022-01-24 15:47:58 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -15,7 +15,15 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React right now
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React right now
         </a>
       </header>
     </div>
