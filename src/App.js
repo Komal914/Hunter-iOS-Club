cdc8d7496ff0dddb8f88e9f45e733813ab03978e
Komal Kaur <44416323+Komal914@users.noreply.github.com>
2022-01-25 22:10:45 -0500

    deleted bad code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import react from "react";
 import Homepage from "./components/pages/homepage/homepage.component";
-import reactRouterDom from "react-router-dom";
+import { Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
