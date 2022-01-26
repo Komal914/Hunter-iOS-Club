acd2cdd1aaf6235a10b9bea5a169aae388c6bf89
Komal Kaur <44416323+Komal914@users.noreply.github.com>
2022-01-25 21:55:32 -0500

    added react router version 5 to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import "./App.css";
+import react from "react";
 import Homepage from "./components/pages/homepage/homepage.component";
+import reactRouterDom from "react-router-dom";
+import "./App.css";
 
 function App() {
   return <div>Hello World!</div>;
