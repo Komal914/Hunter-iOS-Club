ecebf89da5ad7bcf33bd10ced273dacd02636e90
Komal Kaur <44416323+Komal914@users.noreply.github.com>
2022-01-26 22:21:47 -0500

    updated div in contact page

--- src/components/pages/contactuspage/contactuspage.component.jsx
+++ src/components/pages/contactuspage/contactuspage.component.jsx
@@ -1,6 +1,6 @@
 import react from "react";
 import "./contactuspage.styles.scss";
 
-const Contactpage = () => <div>This is the homepage</div>;
+const Contactpage = () => <div>This is the Contactpage.</div>;
 
 export default Contactpage;
